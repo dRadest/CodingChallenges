701fc6b5f97078cd4d84a1802ec43d429ee31fda
DRad <progdrad@gmail.com>
2018-12-14 07:27:17 +0100

    Lonely Integer algorithm challenge on HackerRank

---
+++ HackerRank/Algorithms/Bit Manipulation/LonelyInteger.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the lonelyinteger function below.
+    static int lonelyinteger(int[] a) {
+        // XOR of two equal numbers is 0
+        int result = 0;
+        for(int num : a){
+            result ^= num;
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int result = lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ HackerRank/Algorithms/Bit Manipulation/README.md
@@ -0,0 +1,20 @@
+## Resources
+
+* Wikipedia
+	- [Bitwise Operation](https://en.wikipedia.org/wiki/Bitwise_operation)
+	- [Bitwise Operations in C](https://en.wikipedia.org/wiki/Bitwise_operations_in_C)
+	- [Bit Manipulation](https://en.wikipedia.org/wiki/Bit_manipulation)
+* MDN
+	- [Bit operations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators)
+* envatotuts+
+	- [Understanding Bitwise Operators](https://code.tutsplus.com/articles/understanding-bitwise-operators--active-11301)
+
+## Solutions
+#### Lonely Integer
+[Description](https://www.hackerrank.com/challenges/lonely-integer/problem)
+
+file:
+
+* Java [LonelyInteger.java](LonelyInteger.java)
+* C [lonely_integer.c](lonely_integer.c)
+
