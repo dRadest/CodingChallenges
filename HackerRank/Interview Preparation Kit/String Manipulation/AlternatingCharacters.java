7a1231b97876609ca5094cefb6dfc2b5e392226b
DRad <progdrad@gmail.com>
2018-12-18 12:44:14 +0100

    Alternating Characters interview prep on HackerRank

---
+++ HackerRank/Interview Preparation Kit/String Manipulation/AlternatingCharacters.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // helper method to find the next char
+    static char nextChar(char c){
+        if(c == 'A'){
+            return 'B';
+        }
+        return 'A';
+    }
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+        char firstChar = s.charAt(0);
+        int deletions = 0;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i) != nextChar(firstChar)){
+                deletions++;
+            }else{
+                firstChar = nextChar(firstChar);
+            }
+        }
+        return deletions;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
