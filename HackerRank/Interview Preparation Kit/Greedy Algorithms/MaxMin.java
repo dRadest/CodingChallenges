d043d01349bf64893982b84825571cde5f9e262d
DRad <progdrad@gmail.com>
2018-12-19 12:34:06 +0100

    Max Min interview prep on HackerRank

---
+++ HackerRank/Interview Preparation Kit/Greedy Algorithms/MaxMin.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the maxMin function below.
+    static int maxMin(int k, int[] arr) {
+        // sort the array in ASC order
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        // traverse subarrays of length k
+        for(int i=0; i<=arr.length - k; i++){
+            int unfairness = arr[i+k-1] - arr[i];
+            if(min > unfairness){
+                min = unfairness;
+            }
+        }
+        return min;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            arr[i] = arrItem;
+        }
+
+        int result = maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
