a298a2857c7e12f2fc5c73527df6ec2e4d421175
DRad <progdrad@gmail.com>
2019-02-08 06:50:56 +0100

    Letter Phone solution on InterviewBit

---
+++ Interview Bit/Backtracking/letter_phone.cpp
@@ -0,0 +1,47 @@
+/*
+* https://www.interviewbit.com/problems/letter-phone/
+*
+* Given a digit string, return all possible letter combinations that the number could represent.
+* A mapping of digit to letters (just like on the telephone buttons) is given below.
+*
+* The digit 0 maps to 0 itself.
+* The digit 1 maps to 1 itself.
+*
+* Make sure the returned strings are lexicographically sorted.
+*/
+
+string temp = "";
+unordered_map<char, string> keypad = {
+    { '1', "1" },
+    { '2', "abc" },
+    { '3', "def" },
+    { '4', "ghi" },
+    { '5', "jkl" },
+    { '6', "mno" },
+    { '7', "pqrs" },
+    { '8', "tuv" },
+    { '9', "wxyz" },
+    { '0', "0" }
+};
+void backtracking(string digits, int i, vector<string>& res)
+{
+    if (digits[i] - '0' > -1 && digits[i] - '0' < 10)
+    {    
+        // traverse all possible chars
+        string str = keypad[digits[i]];
+        for (auto j = 0; j<str.length(); ++j)
+        {
+            temp += str[j]; 
+            if (i == digits.length() - 1) // end reached
+                res.emplace_back(temp);
+            else
+                backtracking(digits, i+1, res);
+            temp.pop_back(); // backtrack
+        }
+    }
+}
+vector<string> Solution::letterCombinations(string A) {
+    vector<string> res;
+    backtracking(A, 0, res);
+    return res;
+}
