eea3820a39e0556acf1c30831163352010389516
DRad <progdrad@gmail.com>
2019-02-09 08:29:06 +0100

    Balanced Binary Tree solution on InterviewBit

---
+++ Interview Bit/Tree Data Structure/balanced_binary_tree.cpp
@@ -0,0 +1,70 @@
+/*
+* https://www.interviewbit.com/problems/balanced-binary-tree/
+*
+*Given a binary tree, determine if it is height-balanced.
+*
+*   Height-balanced binary tree : is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1. 
+*
+* Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+*
+* Example :
+* 
+* Input : 
+*          1
+*         / \
+*        2   3
+*
+* Return : True or 1 
+*
+* Input 2 : 
+*         3
+*        /
+*       2
+*      /
+*     1
+*
+* Return : False or 0 
+*         Because for the root node, left subtree has depth 2 and right subtree has depth 0. 
+*         Difference = 2 > 1. 
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int height(TreeNode* node)
+{
+	// base cases
+    if (!node){
+        return 0;
+    }
+    if (!node->left && !node->right){
+        return 1;
+    }
+    
+    // get heights of left and right subtree
+    auto l = 1 + height(node->left);
+    auto r = 1 + height(node->right);
+    
+    // either negative
+    if (l<0 || r<0){
+        return INT_MIN;
+    }
+    // difference shouldn't be more than 1
+    if (abs(l-r)>1){
+        return INT_MIN;
+    }
+    return max(l, r);
+}
+int Solution::isBalanced(TreeNode* A) {
+    if (height(A)<0){
+        return 0;
+    }
+    return 1;
+}
+
