d94f927355114fe4085479a0bcb8e38eb92b79d2
DRad <progdrad@gmail.com>
2019-02-11 13:17:32 +0100

    Gas Station solution on InterviewBit

---
+++ Interview Bit/Greedy Algorithm/gas_station.cpp
@@ -0,0 +1,52 @@
+/*
+*
+* https://www.interviewbit.com/problems/gas-station/
+*
+* There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+*
+* You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). 
+* You begin the journey with an empty tank at one of the gas stations.
+*
+* Return the minimum starting gas station’s index if you can travel around the circuit once, otherwise return -1.
+*
+* You can only travel in one direction. i to i+1, i+2, ... n-1, 0, 1, 2..
+* Completing the circuit means starting at i and ending up at i again.
+*
+* Example :
+*
+*	Input :
+*	      Gas :   [1, 2]
+*	      Cost :  [2, 1]
+*
+*	Output : 1 
+*
+* If you start from index 0, you can fill in gas[0] = 1 amount of gas. Now your tank has 1 unit of gas. 
+* But you need cost[0] = 2 gas to travel to station 1. 
+* If you start from index 1, you can fill in gas[1] = 2 amount of gas. Now your tank has 2 units of gas. 
+* You need cost[1] = 1 gas to get to station 0. So, you travel to station 0 and still have 1 unit of gas left over. 
+* You fill in gas[0] = 1 unit of additional gas, making your current gas = 2. It costs you cost[0] = 2 to get to station 1, which you do and complete the circuit. 
+*/
+
+int Solution::canCompleteCircuit(const vector<int> &A, const vector<int> &B) {
+    int gas = 0, index = 0, sum = 0;
+    
+    // traverse gas stations
+    for(int i = 0; i < A.size(); i++){
+        // add difference in added gas and cost to sum and gas
+        sum += (A[i] - B[i]);
+        gas += (A[i] - B[i]);
+        if(gas < 0){ // reset the starting gas station
+            gas = 0;
+            index = i+1;
+        }
+    }
+    // overall sum of differences has to be above or equal to zero
+    // to be able to make the round
+    if(sum >= 0){
+        return index%(A.size());
+    }
+    
+    return -1;
+            
+}
+
